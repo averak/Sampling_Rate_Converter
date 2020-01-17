b9b6c6729ecb4fe01624da0dd53bf0d43190f2ad
AjxLab <abe12@mccc.jp>
2020-01-18 04:34:15 +0900

    add usage

--- README.md
+++ README.md
@@ -10,3 +10,19 @@ Convert the sampling rate of audio files in .wav format.
 - numpy 1.18.1
 - scipy 1.4.1
 - wave 0.0.2
+
+
+## Usage
+```python
+# read .wav file
+wav, fs = read_wave('wav file path')
+# ex) => wav : [...], fs : 8000
+
+# up sampling (8000 -> 16000)
+up_wav, up_fs = convert_fs(wav, fs, 16000)
+# ex) => up_wav : [...], up_fs : 16000
+
+# down sampling (8000 -> 4000)
+down_wav, down_fs = convert_fs(wav, fs, 4000)
+# ex) => down_wav : [...], down_fs : 4000
+```
--- converter.py
+++ converter.py
@@ -1,62 +1,32 @@
+# -*- coding: utf-8 -*-
 import numpy as np
 import scipy.signal
-import struct
-import wave
+import scipy.io.wavfile
+import os
 
 
-def read_wav(file):
-    ## -----*----- Wavファイルの読み込み -----*----- ##
-    wf = wave.open(file)
-    fs = wf.getframerate()
-
-    wav = np.frombuffer(wf.readframes(wf.getnframes()), dtype="int16")
+def read_wave(wavfile):
+    ## -----*----- .wavファイルの読み込み -----*----- ##
+    fs, wav = scipy.io.wavfile.read(wavfile)
     return (wav, fs)
 
 
-def change_sampling(wav, fs,  change_fs):
-    ## -----*----- Wavファイルの読み込み -----*----- ##
-    # FIRフィルタの用意をする
-    nyqF = fs / 2.0                      # 変換後のナイキスト周波数
-    cF   = (fs / 2.0-500.0)/nyqF         # カットオフ周波数を設定
-    taps = 511                           # フィルタ係数(odd)
-
-    # 変換後の音声を格納
-    converted = []
-
-    if fs < change_fs:
-        # Up Sampling
-        SampleNum = int(change_fs / fs - 1)
-        for d in wav:
-            converted.append(d)
-            for i in range(SampleNum):
-                converted.append(converted[-1])
-
-    elif fs > change_fs:
-        # Down Sampling
-        SampleNum = int(fs / change_fs - 1)
-        for i in range(0, len(wav), SampleNum+1):
-            converted.append(wav[i])
-
-    else:
-        # 変換しない
-        converted = wav
-
-    return (np.array(converted), change_fs)
+def write_wave(wavfile, fs, wav):
+    ## -----*----- .wavファイルに書き込み -----*----- ##
+    if os.path.exists(wavfile):
+        os.remove(wavfile)
+    scipy.io.wavfile.write(wavfile, fs, wav)
 
 
-def write_wav(file, wav, fs):
-    # データを-32768から32767の整数値に変換
-    wav = [int(x) for x in wav]
-    wf = wave.Wave_write(file)
-    b_wave = struct.pack("h" * len(wav), *wav)
-    wf.setparams((
-        1,                          # channel
-        2,                          # byte width
-        fs,                         # sampling rate
-        len(wav),                   # number of frames
-        "NONE", "not compressed"    # no compression
-        )
-    )
-    wf.writeframes(b_wave)
-    wf.close()
+def convert_fs(wav, fs, to_fs, to_int=True):
+    ## -----*----- サンプリングレートを変換 -----*----- ##
+    # wav : 変換対象の音源
+    # fs  : 変換後のサンプリングレート
+
+    converted = scipy.signal.resample(wav, int(wav.shape[0]*to_fs/fs))
+
+    if to_int:
+        converted = np.array(converted, dtype='int16')
+
+    return (converted, to_fs)
 
