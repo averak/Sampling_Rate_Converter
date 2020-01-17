14d056b0dbfc120c2757a897ef6d855198b502e8
AjxLab <abe12@mccc.jp>
2020-01-18 03:11:43 +0900

    サンプリングレートの変換

--- converter.py
+++ converter.py
@@ -1,5 +1,5 @@
 import numpy as np
-import scipy
+import scipy.signal
 import wave
 
 
@@ -8,9 +8,44 @@ def read_wav(file):
     wf = wave.open(file)
     fs = wf.getframerate()
 
-    # -1 ~ 1に正規化
-    data = np.frombuffer(wf.readframes(wf.getnframes()),dtype="int16") / 32768.0
-    return (data, fs)
+    wav = np.frombuffer(wf.readframes(wf.getnframes()), dtype="int16")
+    return (wav, fs)
 
 
-print(read_wav('original.wav'))
+def change_sampling(wav, fs,  change_fs):
+        # FIRフィルタの用意をする
+        nyqF = fs / 2.0                      # 変換後のナイキスト周波数
+        cF   = (fs / 2.0-500.0)/nyqF         # カットオフ周波数を設定
+        taps = 511                           # フィルタ係数(odd)
+        b    = scipy.signal.firwin(taps, cF) # LPFを用意
+
+        # 変換後の音声を格納
+        converted = []
+
+        if fs < change_fs:
+            # Up Sampling
+            SampleNum = int(change_fs / fs - 1)
+            for d in wav:
+                converted.append(d)
+                for i in range(SampleNum):
+                    converted.append(converted[-1])
+            converted = scipy.signal.lfilter(b, 1, converted)
+
+        elif fs > change_fs:
+            # Down Sampling
+            SampleNum = int(fs / change_fs - 1)
+            wav = scipy.signal.lfilter(b, 1, wav)
+            for i in range(0, len(wav), SampleNum+1):
+                converted.append(wav[i])
+
+        else:
+            # 変換しない
+            converted = wav
+
+        return (np.array(converted), change_fs)
+
+
+
+wav, fs = read_wav('original.wav')
+rewav, _ = change_sampling(wav, fs, 16000)
+print(len(wav), len(rewav), rewav)
